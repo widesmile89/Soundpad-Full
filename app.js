10e4ed5993ccb17ee174325e00c4f9af34dda8a4
ibrahimshoeib <hsho2214@gmail.com>
2024-01-22 18:32:22 +0200

    chore : added error handling to the whole project

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.all("*",(req,res,next)=>{
 app.use((err,req,res,next)=>{
     err.statusCode=err.statusCode || 500;
     err.status = err.status || "error"
-    
+
         res.status(err.statusCode).json({
             statusCode:err.statusCode,
             message:err.message,
